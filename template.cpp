7a6b7312ba73d9311d2b1ffa15efb2f9c639f7f8
root <root@DESKTOP-U9VUN16.localdomain>
2022-12-01 18:42:47 -0500

    added template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define pb push_back
+using namespace std; 
+
+int main(void){
+    FILE * fptr = fopen("input.txt", "r"); 
+    char * string = (char*)malloc(sizeof(char) * 10000); 
+    int index = 0; 
+    
+
+    
+    while (!feof(fptr)){
+
+    }
+
+
+
+    fclose(fptr); 
+    free(string); 
+
+
+    return 0; 
+}
