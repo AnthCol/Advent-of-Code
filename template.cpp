98e0acdc2022edc01e05c363708b33758c0d214e
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2023-12-01 10:29:12 -0500

    update template

--- template.cpp
+++ template.cpp
@@ -1,20 +1,13 @@
 #include <bits/stdc++.h>
-#define pb push_back
 using namespace std; 
 
 int main(void){
-    ifstream fptr("input.txt"); 
 
-    string s = ""; 
-    vector<int> vec; 
+    ifstream fptr("input"); 
 
-    int index = 0, sum = 0; 
- 
     while (getline(fptr, s)){
         
     }   
-
-
 
     return 0; 
 }
